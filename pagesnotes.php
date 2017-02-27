ae73cf665759ac39b576d1da251875aaf1d9dad0
Ranack <kevin.roualec@hotmail.fr>
2017-02-27 13:20:47 +0100

    Update pagesnotes.php

--- pagesnotes.php
+++ pagesnotes.php
@@ -1,39 +1,33 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Pages notes</title>
+		<title>Pages notes -        Super projet</title>
 		<meta charset="utf-8">
 	</head>
 
-<p>page de note</p>
+<p>page de notes</p>
 
 <body>
     <table border="2" width="450">
         <tr>
-            <th width="310">Eleve
+            <th width="310">Elève
             <th width="140">Matière
-            <th width="140">Note
+            <th width="140">Notes
         <tr>
             <td>
             <td>
             <td>
-            <td>
-            <td>
-            <td>
+	</tr>	    
         <tr>
             <td>
             <td>
             <td>
-            <td>
-            <td>
-            <td>
+	</tr>	 
         <tr>
             <td>
             <td>
             <td>
-            <td>
-            <td>
-            <td>
+	</tr>	 
     </table>
 </body>
 
