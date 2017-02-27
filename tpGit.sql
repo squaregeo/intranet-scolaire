5c8ea6ac4845446b8f8a127316f012e8ba66b9c0
escalusVI <loicvaleryvi@gmail.com>
2017-02-27 16:10:44 +0100

    ajout de données

--- tpGit.sql
+++ tpGit.sql
@@ -1,60 +1,122 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
 --
---  Création de la base
---
+-- Client: 127.0.0.1
+-- Généré le: Lun 27 Février 2017 à 16:09
+-- Version du serveur: 5.5.27-log
+-- Version de PHP: 5.4.6
 
-DROP DATABASE IF EXISTS TPGit;
-CREATE DATABASE TPGit DEFAULT CHARACTER SET 'utf8';
-USE TPGit;
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
---
---  Création des tables
---
 
-CREATE TABLE Classes (
-    id INT UNSIGNED AUTO_INCREMENT,
-    nom VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
-) ENGINE = INNODB;
-
-CREATE TABLE Eleves (
-    id INT UNSIGNED AUTO_INCREMENT,
-    nom VARCHAR(30) NOT NULL,
-    prenom VARCHAR(30) NOT NULL,
-    naissance DATE NOT NULL,
-    idClasse INT UNSIGNED NOT NULL,
-    PRIMARY KEY (id)
-) ENGINE = INNODB;
-
-CREATE TABLE Matieres (
-    id INT UNSIGNED AUTO_INCREMENT,
-    nom VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
-) ENGINE = INNODB;
-
-CREATE TABLE Notes (
-    id INT UNSIGNED AUTO_INCREMENT,
-    idMatiere INT UNSIGNED NOT NULL,
-    idEleve INT UNSIGNED NOT NULL,
-    note INT NOT NULL,
-    PRIMARY KEY (id)
-) ENGINE = INNODB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
---  Ajout des clés étrangères
+-- Base de données: `tpgit`
 --
 
-ALTER TABLE Eleves
-    ADD CONSTRAINT fk_Eleves_idClasse FOREIGN KEY (idClasse) REFERENCES Classes(id);
-
-ALTER TABLE Notes
-    ADD CONSTRAINT fk_Notes_idMatiere FOREIGN KEY (idMatiere) REFERENCES Matieres(id),
-    ADD CONSTRAINT fk_Notes_idEleve FOREIGN KEY (idEleve) REFERENCES Eleves(id);
+-- --------------------------------------------------------
 
 --
---  Liste des requêtes par page
+-- Structure de la table `classes`
 --
 
--- adminNotes
--- SELECT e.nom, prenom, naissance, c.nom as classe FROM Eleves e INNER JOIN Classes c ON c.id = e.idClasse WHERE e.id = :id;
+CREATE TABLE IF NOT EXISTS `classes` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nom` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
 --
--- SELECT nom as matiere, note FROM Notes n INNER JOIN matieres m ON m.id = n.idMatiere WHERE idEleve = :id;
+-- Contenu de la table `classes`
+--
+
+INSERT INTO `classes` (`id`, `nom`) VALUES
+(1, 'Classe 1'),
+(2, 'classe 2');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `eleves`
+--
+
+CREATE TABLE IF NOT EXISTS `eleves` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nom` varchar(30) NOT NULL,
+  `prenom` varchar(30) NOT NULL,
+  `naissance` date NOT NULL,
+  `idClasse` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_Eleves_idClasse` (`idClasse`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `eleves`
+--
+
+INSERT INTO `eleves` (`id`, `nom`, `prenom`, `naissance`, `idClasse`) VALUES
+(1, 'wayne', 'bruce', '1980-12-10', 1),
+(2, 'kent', 'clark', '1980-08-13', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `matieres`
+--
+
+CREATE TABLE IF NOT EXISTS `matieres` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nom` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `matieres`
+--
+
+INSERT INTO `matieres` (`id`, `nom`) VALUES
+(1, 'Francais'),
+(2, 'Math');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `notes`
+--
+
+CREATE TABLE IF NOT EXISTS `notes` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `idMatiere` int(10) unsigned NOT NULL,
+  `idEleve` int(10) unsigned NOT NULL,
+  `note` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_Notes_idMatiere` (`idMatiere`),
+  KEY `fk_Notes_idEleve` (`idEleve`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `eleves`
+--
+ALTER TABLE `eleves`
+  ADD CONSTRAINT `fk_Eleves_idClasse` FOREIGN KEY (`idClasse`) REFERENCES `classes` (`id`);
+
+--
+-- Contraintes pour la table `notes`
+--
+ALTER TABLE `notes`
+  ADD CONSTRAINT `fk_Notes_idMatiere` FOREIGN KEY (`idMatiere`) REFERENCES `matieres` (`id`),
+  ADD CONSTRAINT `fk_Notes_idEleve` FOREIGN KEY (`idEleve`) REFERENCES `eleves` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
