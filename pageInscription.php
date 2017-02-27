88a1ae245c4c349c9b5e9258174b0b23ac1edb46
redcreep76 <lucien.brouillaud@wanadoo.fr>
2017-02-27 14:12:15 +0100

    Modification Backend Page Inscription
    Modification Backend Page Inscription

--- pageInscription.php
+++ pageInscription.php
@@ -6,30 +6,29 @@
 	</head>
 	
 	<body>
+    <script type="text/javascript" src="jquery.js"></script>
+		<script type="text/javascript" src="main.js"></script>
 		<?php
       $pdo_options[PDO::MYSQL_ATTR_INIT_COMMAND] = "SET NAMES utf8";
-      $pdo = new PDO('mysql:host=localhost;dbname=TPGit', 'root', '', $pdo_options);   
+      $pdo = new PDO('mysql:host=localhost;dbname=TPGit', 'root', '', $pdo_options);    
       
       if (isset($_POST['submit'])) {
+        
         $nom = $_POST['nom'];
         $prenom = $_POST['prenom'];
         $naissance = $_POST['naissance'];
         $idClasse = $_POST['classe'];
-        if (empty($nom)) {
-          echo "<h3>Vous devez entrer le nom de l'élève.</h3>";
-          
-        } else if (empty($prenom)) {
-          echo "<h3>Vous devez entrer le prénom de l'élève.</h3>";
-        } else {
-          $pdo->query('INSERT INTO eleves (nom, prenom, naissance, idClasse) VALUES ("'.$nom.'", "'.$prenom.'", "'.$naissance.'", "'.$idClasse.'")');
-        }
+        $pdo->query('INSERT INTO eleves (nom, prenom, naissance, idClasse) VALUES ("'.$nom.'", "'.$prenom.'", "'.$naissance.'", "'.$idClasse.'")');
+        echo "<h3>Vous venez d'enregistrer l'élève ".$prenom." ".$nom.".</h3>";
+        echo "<a href='pageInscription.php'><button>Retour</button></a>";
+        return;
       }
 		?>
     
-    <form method="post">
-      <input type="text" name="nom" placeholder="Exemple: Simpson" value="<?php echo $nom; ?>" />
+    <form method="post" onsubmit="return validate();">
+      <input type="text" name="nom" placeholder="Exemple: Simpson" />
       <input type="text" name="prenom" placeholder="Exemple: Homer" />
-      <input type="date" name="naissance" value="<?php echo date('Y-m-d'); ?>" />   
+      <input type="date" name="naissance" value="<?php echo date('Y-m-d'); ?>"/>   
       <select name="classe">
         <?php
           foreach ($pdo->query("SELECT * FROM classes") as $row) {
