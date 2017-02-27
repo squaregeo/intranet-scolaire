f4b097969d279f0d8213484a7fa2399492f2b99b
escalusVI <loicvaleryvi@gmail.com>
2017-02-27 15:09:27 +0100

    ajout fichier accueil et dossier de style

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+		<div id="haut">
+				<p><img src="img/logo_ecole.png" alt="logo_ecole" height="200" width="0"/></p>
+		</div>
+		
+		<header>
+				<ul>
+					<li><a href="accueil.php">Accueil</a></li>
+					<li><a href="inc_dbconnexion.php">Inscription</a></li>
+					<li><a href="pageInscription.php">Notes</a></li>
+					<li><a href="pagesnotes.php">Consultation notes</a></li>
+					<li><a href="adminNote.php">Administration</a></li>
+
+				</ul>
+		</header>
