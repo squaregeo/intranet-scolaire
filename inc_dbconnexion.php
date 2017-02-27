cd21d8220fdadb807a10a3441a4f33adab5baa7a
BreathingLink <anthony.se@hotmail.fr>
2017-02-27 13:30:45 +0100

    fichier de connexion à la bdd
    A inclure dans les fichiers backends pour créer une connexion à la base
    de données. La variable $pdo est créée.

--- inc_dbconnexion.php
+++ inc_dbconnexion.php
@@ -1,3 +1,13 @@
 <?php
-
-?>
+    // Tente une connexion à la base de données
+    // Les erreurs sql lanceront des erreurs php
+    // Pas besoin d'écrire fetch(PDO::FETCH_ASSOC), c'est fait par défaut
+    try {
+        $pdo_options[PDO::MYSQL_ATTR_INIT_COMMAND] = "SET NAMES utf8";
+        $pdo = new PDO('mysql:host=localhost;dbname=TPGit', 'root', '', $pdo_options);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    }
+    catch (PDOException $e) {
+        die($e->getMessage());
+    }
