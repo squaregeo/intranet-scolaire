526aa1e60e5c8f1fda0c6aea6b0d887bd845d19c
Ranack <kevin.roualec@hotmail.fr>
2017-02-27 15:36:36 +0100

    Update and rename pagesnotes.php to 404.php

---
+++ 404.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>404- Not Found</title>
+		<meta charset="binary">
+	</head>
+
+
+
+<body>
+
+		<video src="codecul.mp4" autoplay loop style="width: 450px; height:450px;"></video>
+			
+	<p>404</p><br/><br/>
+				<p>The page you're looking for could not be found.</p>
+				
+					<a href="accueil.php" class="more go">Go back</a>
+				
+	
+</body>
+
+</html>
