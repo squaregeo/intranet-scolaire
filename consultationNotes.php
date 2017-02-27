d35cfe9203803035310f52426fedd93f5c386227
escalusVI <loicvaleryvi@gmail.com>
2017-02-27 14:46:58 +0100

    liste eleves en menu deroulant

--- consultationNotes.php
+++ consultationNotes.php
@@ -1,1 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Consultation des notes</title>
+</head>
+<body>
+	<?php include ("inc_dbconnexion.php");
+	$reponse = $pdo->prepare('SELECT * FROM Notes');
+	$reponse->fetch();
+	?>
+
+	<h1>Page de consultation des notes</h1>
+	<p>Vous pouvez ici consulter vos notes</p>
+
+	<p>
+	<select>Veuilez-vous identifier<br/></select>
+		<?php foreach($reponse as $donnees) 
+		{?>
+			<option value="<?php $donnees['idEleve']; ?>"><?php echo $donnees["eleves"];?></option>	
+		<?php
+		}
+		?>
+
+
+		</p>
+		<?php $reponse->closeCursor(); ?>
+</body>
+</html>
 
