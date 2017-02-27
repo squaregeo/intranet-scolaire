7a0b7cf037aab8d198dbf70a4bf26d3a1317f574
albanguerbette <aguerbette@gmail.com>
2017-02-27 13:16:42 +0100

    ajout label + stylesheet + class dans pageinscription.php

--- pageInscription.php
+++ pageInscription.php
@@ -2,42 +2,53 @@
 <html>
 	<head>
     <title>Administration</title>
-		<meta charset="utf-8"></meta>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/pure-min.css">
+		<link rel="stylesheet" href="style.css" />
+    <meta charset="utf-8"></meta>
 	</head>
 	
 	<body>
-		<?php
-      $pdo_options[PDO::MYSQL_ATTR_INIT_COMMAND] = "SET NAMES utf8";
-      $pdo = new PDO('mysql:host=localhost;dbname=TPGit', 'root', '', $pdo_options);   
-      
-      if (isset($_POST['submit'])) {
-        $nom = $_POST['nom'];
-        $prenom = $_POST['prenom'];
-        $naissance = $_POST['naissance'];
-        $idClasse = $_POST['classe'];
-        if (empty($nom)) {
-          echo "<h3 class=\"alert\">Vous devez entrer le nom de l'élève.</h3>";
+
+    <div class="inscription-wrapper">
+
+    <?php
+          $pdo_options[PDO::MYSQL_ATTR_INIT_COMMAND] = "SET NAMES utf8";
+          $pdo = new PDO('mysql:host=localhost;dbname=TPGit', 'root', '', $pdo_options);   
           
-        } else if (empty($prenom)) {
-          echo "<h3 class=\"alert\">Vous devez entrer le prénom de l'élève.</h3>";
-        } else {
-          $pdo->query('INSERT INTO eleves (nom, prenom, naissance, idClasse) VALUES ("'.$nom.'", "'.$prenom.'", "'.$naissance.'", "'.$idClasse.'")');
-        }
-      }
-		?>
-    
-    <form class="form__inscription" method="post">
-      <input class="input__inscription" type="text" name="nom" placeholder="Exemple: Simpson" value="<?php echo $nom; ?>" />
-      <input class="input__inscription" type="text" name="prenom" placeholder="Exemple: Homer" />
-      <input class="input__inscription" type="date" name="naissance" value="<?php echo date('Y-m-d'); ?>" />   
-      <select name="classe">
-        <?php
-          foreach ($pdo->query("SELECT * FROM classes") as $row) {
-            echo "<option value='".$row['id']."'>".$row['nom']."</option>";
+          if (isset($_POST['submit'])) {
+            $nom = $_POST['nom'];
+            $prenom = $_POST['prenom'];
+            $naissance = $_POST['naissance'];
+            $idClasse = $_POST['classe'];
+            if (empty($nom)) {
+              echo "<h3 class=\"alert\">Vous devez entrer le nom de l'élève.</h3>";
+              
+            } else if (empty($prenom)) {
+              echo "<h3 class=\"alert\">Vous devez entrer le prénom de l'élève.</h3>";
+            } else {
+              $pdo->query('INSERT INTO eleves (nom, prenom, naissance, idClasse) VALUES ("'.$nom.'", "'.$prenom.'", "'.$naissance.'", "'.$idClasse.'")');
+            }
           }
-        ?>
-      </select>  
-      <button class="btn__inscription" type="submit" name="submit">Envoyez</button>
-    </form>
-	</body>
+    ?>
+        
+        <form class="pure-form pure-form-stacked" method="post">
+          <label for="nom">Nom</label>
+          <input type="text" id="nom" name="nom" placeholder="Exemple: Simpson" value="<?php echo $nom; ?>" />
+          <label for="prenom">Prénom</label>
+          <input type="text" id="prenom"  name="prenom" placeholder="Exemple: Homer" />
+          <label for="naissance">Naissance</label>
+          <input type="date" id="naissance" name="naissance" value="<?php echo date('Y-m-d'); ?>" />   
+          <label for="classe">Classe</label>
+          <select id="classe" name="classe">
+            <?php
+              foreach ($pdo->query("SELECT * FROM classes") as $row) {
+                echo "<option value='".$row['id']."'>".$row['nom']."</option>";
+              }
+            ?>
+          </select>  
+          <button class="pure-button pure-button-primary" type="submit" name="submit">Envoyez</button>
+        </form>
+    </div>
+  </body>
 </html>
